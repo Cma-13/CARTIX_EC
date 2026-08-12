5333a96faaaa9ce84f5d4a399e940504a9df8d60
Cma-13 <aasmasingdan77@gmail.com>
2026-08-12 12:13:36 +0545

    feat: add product grid with fetching and loading/error states

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,9 @@
-import { useEffect, useState } from 'react';
+import ProductList from "../pages/ProductList";
 
 function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/api/products/')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-      .catch(error => console.error(error));
-  }, []);
-
   return (
-    <div className="min-h-screen bg-gray-100 test-gray-800">
-      <h1 className='text-3xl font-bold underline'>Product List</h1>
-      <div className='container mx-auto p-4'>
-        {products.map(product => (
-          <div key={product.id} className='bg-white rounded-lg shadow-md p-4 mb-4'>
-            <h2 className='text-xl font-bold mb-2'>{product.name}</h2>
-            <p className='text-gray-700 mb-2'>Price: Rs.{product.price}</p>
-            <p className='text-gray-700 mb-2'>Description: {product.description}</p>
-          </div>
-        ))}
-      </div>
+    <div>
+      <ProductList />
     </div>
   );
 }
