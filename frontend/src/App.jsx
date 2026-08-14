1458caccf164189d048f0d27ed32753ef297ff61
Cma-13 <aasmasingdan77@gmail.com>
2026-08-14 13:37:14 +0545

    feat(cart): add cart context and provider setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import ProductList from "./pages/ProductList.jsx";
 import ProductDetail from "./pages/ProductDetails.jsx";
+import Navbar from "./components/Navbar.jsx";
+import CartPage from "./pages/CartPage.jsx";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<ProductList />} />
         <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<CartPage />} />
+
       </Routes>
     </Router>
 
