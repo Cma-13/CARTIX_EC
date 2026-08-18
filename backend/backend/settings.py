5ba43c4d87d696c511b45ce4007839d6c53e0cbb
Cma-13 <aasmasingdan77@gmail.com>
2026-08-18 12:38:51 +0545

    feat(auth): set up JWT auth backend with user registration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from datetime import timedelta
+
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'store',
     'corsheaders',
 ]
@@ -142,3 +145,20 @@ CORS_ALLOWED_ORIGINS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS' : False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
--- backend/store/views.py
+++ backend/store/views.py
@@ -1,7 +1,10 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth.models import User
+from rest_framework import status
 from .models import Category, Product, Cart, CartItem, Order, OrderItem
-from .serializers import CategorySerializer, ProductSerializer, CartSerializer, CartItemSerializer
+from .serializers import CategorySerializer, ProductSerializer, CartSerializer, CartItemSerializer, RegisterSerializer, UserSerializer
 
 @api_view(['GET'])
 def get_products(request):
@@ -26,16 +29,18 @@ def get_categories(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_cart(request):
-    cart, created = Cart.objects.get_or_create(user=None)
+    cart, created = Cart.objects.get_or_create(user=request.user)
     serializer = CartSerializer(cart)
     return Response(serializer.data)
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def add_to_cart(request):
     product_id = request.data.get('product_id')
     product = Product.objects.get(id=product_id)
-    cart, created = Cart.objects.get_or_create(user=None)
+    cart, created = Cart.objects.get_or_create(user=request.user)
     item, created = CartItem.objects.get_or_create(cart=cart, product=product)
     if not created:
         item.quantity += 1
@@ -44,6 +49,7 @@ def add_to_cart(request):
     return Response({'message': 'Product added to cart', 'cart': CartSerializer(cart).data})
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def update_cart_quantity(request):
     item_id = request.data.get('item_id')
     quantity = request.data.get('quantity')
@@ -66,55 +72,71 @@ def update_cart_quantity(request):
       
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def remove_from_cart(request):
     item_id = request.data.get('item_id')
     CartItem.objects.filter(id=item_id).delete()
     return Response({'message': 'Item removed from cart'})
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def create_order(request):
     try:
         data = request.data
-        
         name = data.get('name')
         address = data.get('address')
         phone = data.get('phone')
         payment_method = data.get('payment_method','COD')
         
-        cart = Cart.objects.first()
         
-        if not cart or not cart.items.exists():
+        #VALIDATE PHONE NUMBER
+        if not phone.isdigit() or len(phone) < 10:
+            return Response({'error': 'Invalid phone number'}, status=400)
+        
+        #Get user's cart
+        cart , created = Cart.objects.get_or_create(user=request.user)
+        if not cart.items.exists():
             return Response({'error': 'Cart is empty'}, status=400)
         
-        
-        total = sum(float(item.product.price) * item.quantity for item in cart.items.all())
-        
-        # Create order
+        total = sum(item.product.price * item.quantity for item in cart.items.all())
         
         order = Order.objects.create(
-            user = None,
-            total_amount = total,
+           user = request.user,
+           total_amount = total,
         )
         
-        
-        # Create order items
         for item in cart.items.all():
             OrderItem.objects.create(
                 order = order,
                 product = item.product,
                 quantity = item.quantity,
-                price = item.product.price,
+                price = item.product.price
             )
         
-        # Clear cart
+        #Clear the cart
         cart.items.all().delete()
-        
-        return Response({
-            'message': 'Order placedsuccessfully',
-            'order_id': order.id,
-        })
-        
+        return Response({'message': 'Order created successfully','order_id': order.id})
     except Exception as e:
         return Response({'error': str(e)}, status=500)
+        
+        
+        
+        
+    
+
+    
+    
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_view(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response({'message': 'User registered successfully',"user": UserSerializer(user).data}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
         
         
